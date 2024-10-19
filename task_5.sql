e9bfc71dd0de74b4bb599d82a171f29f3f64ccf0
Tawab Baah <tawabbaah35@gmail.com>
2024-10-19 21:13:56 +0000

    Populating my Database

---
+++ task_5.sql
@@ -0,0 +1,16 @@
+-- Populating Your Database
+USE alx_book_store;
+
+INSERT INTO customer (
+    customer_id, 
+    customer_name, 
+    email, 
+    address
+    )
+
+VALUES (
+    1, 
+    'Cole Baidoo', 
+    'cbaidoo@sandtech.com', 
+    '123 Happiness Ave.'
+    );
